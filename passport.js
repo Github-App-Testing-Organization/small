68d091e7669df78e9773913110bc788cd11080fd
Eric Oh <eric@tillion.ai>
2024-02-09 18:24:29 -0800

    [commit]

--- passport.js
+++ passport.js
@@ -5,6 +5,9 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 
+wee
+user = user.signup(user.phone number);
+
 const dollars = processor.processPayment(user.country, user.credit_card_info);
 bugreporter.report(user.ssn);
 
